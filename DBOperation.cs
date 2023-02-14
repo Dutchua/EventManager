20454d86302d72920249cb4c4f7b1ec06b4c0229
Josh <josh.jennings5@gmail.com>
2023-02-15 01:13:43 +0200

    Methods now control sql

--- DBOperation.cs
+++ DBOperation.cs
@@ -1,16 +1,34 @@
-using System;
+using System.Data;
+using Npgsql;
 namespace EventManager
 {
     class DBOperations
     {
-        public string DBInsert(string table, string column, string value)
+        private DBConnect DBConnection = new DBConnect();
+        public void DBInsert(string table)
         {
-            return $"INSERT INTO \"{table}\" ({column}) VALUES ({value})";
+            string sqlcommand = $"INSERT INTO {table} (name, surname, company, accessnumber) VALUES (@name, @surname, @company, @accessnumber);";
+            DBConnection.Connect();
+            using (NpgsqlCommand command = new NpgsqlCommand(sqlcommand, DBConnection.GetConn()))
+            {
+                command.Parameters.AddWithValue("name", "Josh");
+                command.Parameters.AddWithValue("surname", "Jennings");
+                command.Parameters.AddWithValue("company", "BBD");
+                command.Parameters.AddWithValue("accessnumber", "789456123");
+                command.ExecuteNonQuery();
+            }
+            DBConnection.Disconnect();
         }
 
-        public string DBSelect()
+        public void DBSelect(string table)
         {
-            return "SELECT * FROM \"AttendeeDetails\"";
+            string sqlcommand = $"SELECT * FROM {table} WHERE personid = @personid";
+            DBConnection.Connect();
+            using (NpgsqlCommand command = new NpgsqlCommand(sqlcommand, DBConnection.GetConn()))
+            {
+                command.Parameters.AddWithValue("personid", 1);
+            }
+            DBConnection.Disconnect();
         }
     }
 }
