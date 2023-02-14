a7bbca949e77cabb9640f1a1b773ea0c646aa456
Josh <josh.jennings5@gmail.com>
2023-02-14 20:01:36 +0200

    Created Connection to DB

---
+++ DBConnect.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+using System;
+namespace EventManager
+{
+    class DBConnect
+    {
+        private NpgsqlConnection conn;
+
+        public async void connection()
+        {
+            var connString = "Host='localhost';Username='postgres';Password='123456';Database='PropertyManager";
+            await using conn = new NpgsqlConnection(connString);
+            await conn.OpenAsync()
+        }
+
+        public async void CloseConnection()
+        {
+            await conn.CloseAsync()
+        }
+    }
+}
