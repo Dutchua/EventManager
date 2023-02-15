a36b19142a3a9714276a0d33721e2e91670b9836
Josh <josh.jennings5@gmail.com>
2023-02-15 02:32:54 +0200

    Implemented stored data

--- DBOperation.cs
+++ DBOperation.cs
@@ -5,28 +5,28 @@ namespace EventManager
     class DBOperations
     {
         private DBConnect DBConnection = new DBConnect();
-        public void DBInsert(string table)
+        public void DBInsert(string table, OperationsManager opsManager)
         {
             string sqlcommand = $"INSERT INTO {table} (name, surname, company, accessnumber) VALUES (@name, @surname, @company, @accessnumber);";
             DBConnection.Connect();
             using (NpgsqlCommand command = new NpgsqlCommand(sqlcommand, DBConnection.GetConn()))
             {
-                command.Parameters.AddWithValue("name", "Josh");
-                command.Parameters.AddWithValue("surname", "Jennings");
-                command.Parameters.AddWithValue("company", "BBD");
-                command.Parameters.AddWithValue("accessnumber", "789456123");
+                command.Parameters.AddWithValue("name", opsManager.name);
+                command.Parameters.AddWithValue("surname", opsManager.surname);
+                command.Parameters.AddWithValue("company", opsManager.company);
+                command.Parameters.AddWithValue("accessnumber", opsManager.accessnumber);
                 command.ExecuteNonQuery();
             }
             DBConnection.Disconnect();
         }
 
-        public void DBSelect(string table)
+        public void DBSelect(string table, OperationsManager opsManager)
         {
             string sqlcommand = $"SELECT * FROM {table} WHERE personid = @personid";
             DBConnection.Connect();
             using (NpgsqlCommand command = new NpgsqlCommand(sqlcommand, DBConnection.GetConn()))
             {
-                command.Parameters.AddWithValue("personid", 1);
+                command.Parameters.AddWithValue("personid", opsManager.personID);
             }
             DBConnection.Disconnect();
         }
