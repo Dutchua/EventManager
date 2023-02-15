4e5429e5663c2f2c00eaacad59468ae83c42641c
Josh <josh.jennings5@gmail.com>
2023-02-15 02:32:26 +0200

    Able to store data

--- EventManager.cs
+++ EventManager.cs
@@ -14,7 +14,11 @@ namespace EventManager
         public void Run()
         {
             DBOperations ops = new DBOperations();
-            ops.DBSelect("AttendeeDetails");
+            OperationsManager opmanager = new OperationsManager();
+            opmanager.name = "Josh";
+            opmanager.surname = "Jennings";
+            opmanager.company = "BBD";
+            opmanager.accessnumber = "5125591f-e21b-4e1e-9c6b-3f65606ed79d";
         }
     }
 }
---
+++ OperationsManager.cs
@@ -0,0 +1,11 @@
+namespace EventManager
+{
+    class OperationsManager
+    {
+        public int personID {get;set;}
+        public string name {get;set;}
+        public string surname {get;set;}
+        public string company {get;set;}
+        public string accessnumber {get;set;}
+    }
+}
