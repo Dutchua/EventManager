700fd7c5788f1785feaec75f3ded7bc36e6f1415
Josh <josh.jennings5@gmail.com>
2023-02-15 02:37:38 +0200

    Get data from "user"

--- EventManager.cs
+++ EventManager.cs
@@ -14,11 +14,19 @@ namespace EventManager
         public void Run()
         {
             DBOperations ops = new DBOperations();
+
+
+        }
+
+        public OperationsManager getData()
+        {
             OperationsManager opmanager = new OperationsManager();
+            opmanager.personID = 1;
             opmanager.name = "Josh";
             opmanager.surname = "Jennings";
             opmanager.company = "BBD";
             opmanager.accessnumber = "5125591f-e21b-4e1e-9c6b-3f65606ed79d";
+            return opmanager;
         }
     }
 }
